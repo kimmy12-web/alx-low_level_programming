b0ee40bc41c70e6d8ed72b374e21cb4b04c3f29e
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-10 10:29:29 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include stdio.h
+#include <stdio.h>
+
+/**
+ * main - main function
+ */
 
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
+/*Return: returns zero*/
 }
