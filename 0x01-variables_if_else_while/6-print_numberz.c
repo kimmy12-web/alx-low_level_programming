bff400fa1f2509ea35c979b2cd91d16a7cfcd786
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-11 04:55:32 -0800

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,13 @@
 
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <10; i++)
-	{
-		putchar(i);
-	}
+	for (num = 0; num < 10; num++)
+
+		putchar((num % 10) +'0');
+
+
 	putchar('\n');
 	return (0);
 }
