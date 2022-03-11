a545c2f487ad90f733fe8da71997399859100ad1
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-11 09:05:13 -0800

    combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,48 +9,29 @@
 */
 
 int main(void)
-
 {
+	int i, j;
 
-	int num1, num2;
-
-
-	for (num1 = 0; num1 <= 98; num1++)
-
+	for (i = 0; i <= 99; i++)
 	{
-
-		for (num2 = num1 + 1; num2 <= 99; num2++)
-
+		for (j = 0; j <= 99; j++)
 		{
+			if (i < j)
+			{
+				putchar ((i / 10) + '0');
+				putchar ((i % 10) + '0');
+				putchar (' ');
+				putchar ((j / 10) + '0');
+				putchar ((j % 10) + '0');
 
-			putchar((num1 / 10) + '0');
-
-			putchar((num1 % 10) + '0');
-
-			putchar(' ');
-
-			putchar((num2 / 10) + '0');
-
-			putchar((num2 % 10) + '0');
-
-
-			if (num1 == 98 && num2 == 99)
-
-				continue;
-
-
-			putchar(',');
-
-			putchar(' ');
-
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-
 	}
-
-
 	putchar('\n');
-
-
 	return (0);
-
 }
