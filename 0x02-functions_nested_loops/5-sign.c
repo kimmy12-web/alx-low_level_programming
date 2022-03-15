0249b0ed852b9378525e885c1fd6cba9471c8a1e
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-15 14:02:59 -0700

    sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@
 
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
 		return (0);
 	}
 	else
