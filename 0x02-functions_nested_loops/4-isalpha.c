1292da82f9872bbf19a75d346f55d3ded540670d
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-15 12:47:37 -0700

    is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_isalpha - checks for alphabetic character
+ *@c : the character to be checked
+ *
+ *Return: 1 if c is a letter, lowercase or uppercase, otherwise 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
