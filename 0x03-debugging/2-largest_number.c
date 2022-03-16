b888eb79504bd253e1b6834b970b61c382000043
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-16 14:38:34 -0700

    addition

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0
+*/
+
+int main(void)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (0);
+}
