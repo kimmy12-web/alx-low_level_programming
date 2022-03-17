8ccf28f3f5337e6166b05143bb60e49e99da3872
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-17 03:38:22 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  *print_numbers - prints numbers
- *Return: no return
+ *Return: void
 */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (ch = '0'; c < '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
