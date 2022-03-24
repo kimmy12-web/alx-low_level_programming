ec8dca344b0659d93108709ff2d964b9c9177c1b
kimmy12-web <kimmymubiru12@gmail.com>
2022-03-24 02:48:29 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * infinite_add - adds two numbers
  * @n1: number one.
@@ -7,7 +8,7 @@
  * @r: buffer that the function will use to store the result.
  * @size_r: buffer size:
  * Return: the pointer to dest.
- */
+*/
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
