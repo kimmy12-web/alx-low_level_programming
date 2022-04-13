2774983010763f2a024d3d219eb9f1c5bf535fb2
kimmy12-web <kimmymubiru12@gmail.com>
2022-04-13 21:03:41 +0300

    names

--- function_pointers.h
+++ function_pointers.h
@@ -1,9 +1,9 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
-#include <stdlib.h>
+#include <stddef.h>
 
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 
-#endif /*
-
+#endif /* _FUNCTION_POINTERS_H_ */
